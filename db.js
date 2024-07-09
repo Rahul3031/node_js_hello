31338b500df6bc000fa56095351c9779bfdd3fc0
Rahul Tomar <rahulassan1@gmail.com>
2024-07-09 15:22:37 +0530

    chnage undo

--- db.js
+++ db.js
@@ -1,24 +1,20 @@
 import mongoose from "mongoose";  // Import mongoose to connect the DB server with the actual server
 import dotenv from "dotenv";
+
 dotenv.config();
 // const mongoUrl = process.env.MONGO_URL_LOCAL;
 const mongoUrl = process.env.MONGO_URL;
 
-mongoose.connect(mongoUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    ssl: true, // Enables SSL
-    sslValidate: true, // Validates the SSL certificate
-    tlsInsecure: true // Allows insecure connections (use with caution)
-}).then(() => {
-    console.log('DB is connected');
-}).catch((err) => {
-    console.error('DB connection error:', err);
+mongoose.connect(mongoUrl, {       // Make connection to MongoDB URL
+    useNewUrlParser: true,         // This option is deprecated in newer Mongoose versions
+    useUnifiedTopology: true       // This option is deprecated in newer Mongoose versions
 });
 
 const db = mongoose.connection;    // db object now going to use
 
-
+db.on('connected', () => {         // Some inbuilt functions of mongoose
+    console.log("DB is connected");
+});
 
 db.on('disconnected', () => {
     console.log("DB is disconnected");
