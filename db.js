211f5e709a5bf079722a48fccce77c192178685b
Rahul Tomar <rahulassan1@gmail.com>
2024-07-09 15:18:01 +0530

    add some mongoose connection

--- db.js
+++ db.js
@@ -4,16 +4,21 @@ dotenv.config();
 // const mongoUrl = process.env.MONGO_URL_LOCAL;
 const mongoUrl = process.env.MONGO_URL;
 
-mongoose.connect(mongoUrl, {       // Make connection to MongoDB URL
-    useNewUrlParser: true,         // This option is deprecated in newer Mongoose versions
-    useUnifiedTopology: true       // This option is deprecated in newer Mongoose versions
+mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true, // Enables SSL
+    sslValidate: true, // Validates the SSL certificate
+    tlsInsecure: true // Allows insecure connections (use with caution)
+}).then(() => {
+    console.log('DB is connected');
+}).catch((err) => {
+    console.error('DB connection error:', err);
 });
 
 const db = mongoose.connection;    // db object now going to use
 
-db.on('connected', () => {         // Some inbuilt functions of mongoose
-    console.log("DB is connected");
-});
+
 
 db.on('disconnected', () => {
     console.log("DB is disconnected");
